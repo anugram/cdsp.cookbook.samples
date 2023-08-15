9396fba20b4df4cc6f3c14d2b6173f78a3cdfb2b
anugram <anurag.jain@thalesgroup.com>
2023-08-15 14:16:58 -0400

    fix .env

--- DPG_Banking_PCI_DSS/frontend/entrypoint.sh
+++ DPG_Banking_PCI_DSS/frontend/entrypoint.sh
@@ -3,5 +3,5 @@ url=`printenv API_IP`
 port=`printenv API_PORT`
 truncate -s 0 .env
 echo "REACT_APP_BACKEND_IP_ADDRESS=$url" >> .env
-echo "REACT_APP_BACKEND_PORT=$port" > .env
+echo "REACT_APP_BACKEND_PORT=$port" >> .env
 npm run start
